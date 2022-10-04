9fb559ea15551e1e2c5404feb4f201e0cfc65ab6
zzeho1016 <zzeho1016@gmail.com>
2022-10-04 21:41:57 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/test/BlogControllerTest.java
@@ -0,0 +1,14 @@
+ package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 특정 어노테이션이 붙어있는 클래스 파일들을 new 해서 IOC 스프링 컨테이너에서 관리
+public class BlogControllerTest {
+
+	@GetMapping("/test/hello")
+	public String hello() {
+		return "<h1>hello spring boot</h1>";
+	}
+	
+}
